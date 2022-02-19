09e0a6306e1e5d49d1a4f1601f197b9ca1c4871a
Zenater <zent230096@mail.ru>
2022-02-19 16:37:59 +0300

    hw5 code review

--- src/p2-homeworks/h5/ComponentsRoutes.tsx
+++ src/p2-homeworks/h5/ComponentsRoutes.tsx
@@ -10,7 +10,6 @@ export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
     JUNIOR_PLUS: '/junior-plus',
-    // add paths
 }
 
 function ComponentsRoutes() {
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -6,17 +6,14 @@ import s from "./Header.module.css"
 function Header() {
     return (
         <div className={s.header}>
-            <NavLink to={PATH.PRE_JUNIOR}  className={({isActive}) =>  isActive ? s.active : 's.link'}> pre_junior---</NavLink>
-            <NavLink to={PATH.JUNIOR}  className={({isActive}) =>  isActive ? s.active : s.link} >junior---</NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) =>  isActive ? s.active : s.link} > junior+---</NavLink>
-            <div className={s.block}/>
+            <NavLink to={PATH.PRE_JUNIOR}  className={({isActive}) =>  isActive ? s.active : 's.link'}> pre_junior</NavLink>
+            <NavLink to={PATH.JUNIOR}  className={({isActive}) =>  isActive ? s.active : s.link} >junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) =>  isActive ? s.active : s.link} > junior+</NavLink>
+        <div className={s.div}>
 
-            {/*<NavLink  to='/profile' className={({isActive}) =>  isActive ? s.gold : s.white}>Profile</NavLink>*/}
-            {/*<NavLink  to='/profile' className={({isActive}) =>  isActive ? s.active : s.link}>Profile</NavLink>*/}
+        </div>
         </div>
     )
 }
-// className={s.header}
-// activeClassName={s.active}
-// className={s.link}
+
 export default Header
