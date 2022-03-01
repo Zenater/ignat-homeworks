f7625aeda3bc418d1c46d8a3afd16cdc2f0f4190
Zenater <zent230096@mail.ru>
2022-03-01 22:20:04 +0300

    executed HW10

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './p1-main/m1-ui/u1-app/App'
-import * as serviceWorker from './serviceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import {Provider} from "react-redux";
+import {store} from "./p2-homeworks/h10/bll/store";
+import App from "./p1-main/m1-ui/u1-app/App";
 
-ReactDOM.render(
-    <React.StrictMode>
+
+ReactDOM.render(<BrowserRouter>
+    <Provider store={store}>
         <App/>
-    </React.StrictMode>,
-    document.getElementById("root")
-)
+    </Provider>
+</BrowserRouter>,document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+
+
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
-
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingType): typeof initState=> { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'ADD-LOADING': {
+            return {...state,
+
+                isLoading: action.isLoading}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading:boolean)=> {
+    return  { type: 'ADD-LOADING',    isLoading} as const
+}
+
+type LoadingType = ReturnType<typeof loadingAC>
